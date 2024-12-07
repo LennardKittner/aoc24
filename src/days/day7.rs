85a31607cb517c231de2c9398fbf4a52b3ff95af
LennardKittner <lennard@kittner.dev>
2024-12-07 13:24:37 +0100

    faster day7

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -2,7 +2,7 @@
 use std::fs;
 use std::str::FromStr;
 use itertools::Itertools;
-use rayon::iter::{IntoParallelRefIterator, ParallelBridge};
+use rayon::iter::IntoParallelRefIterator;
 use crate::days::day7::Operator::{Addition, Concat, Multiply};
 use rayon::iter::ParallelIterator;
 
@@ -13,26 +13,32 @@ enum Operator {
     Concat
 }
 
-pub fn exec_day7_part1(input: &str) -> String {
-    let equations = input.lines().map(|l| {
-        let numbers = l.split_whitespace().collect_vec();
-        let mut result = numbers[0].to_string();
-        result.pop();
-        let result = u64::from_str(&result).unwrap();
-        let rest = numbers.iter().rev().take(numbers.len()-1).map(|n| u64::from_str(n).unwrap()).rev().collect_vec();
-        (result, rest)
-    }).collect_vec();
-    let max_len = equations.iter().map(|(_, rest)| rest.len()).max().unwrap();
-    let mut operators = Vec::new();
-    for _ in 0..max_len {
-        operators.push(Multiply);
-        operators.push(Addition);
+fn check1(operator: Operator, end_result: u64, current_result: u64, operants: &[u64]) -> bool {
+    if end_result == current_result && operants.is_empty() {
+        return true;
     }
+    if end_result < current_result || operants.is_empty() {
+        return false;
+    }
+    match operator {
+        Multiply => {
+            check1(Multiply, end_result, current_result * operants[0], &operants[1..]) ||
+                check1(Addition, end_result, current_result * operants[0], &operants[1..])
+        }
+        Addition => {
+            check1(Multiply, end_result, current_result + operants[0], &operants[1..]) ||
+                check1(Addition, end_result, current_result + operants[0], &operants[1..])
+        }
+        _ => panic!()
+    }
+}
+
+pub fn exec_day7_part1(input: &str) -> String {
+    let equations = parse_input(input);
 
     equations.par_iter().map(|(result, operants)| {
-        if (0..2u64.pow(operants.len() as u32)).par_bridge().any(|ops| {
-            evaluate(&generate_operator_vec(ops, operants.len()), operants.as_slice()) == *result
-        }) {
+        if check1(Multiply, *result, operants[0], &operants[1..]) ||
+            check1(Addition, *result, operants[0], &operants[1..]) {
             *result
         } else {
             0
@@ -40,72 +46,54 @@ pub fn exec_day7_part1(input: &str) -> String {
     }).sum::<u64>().to_string()
 }
 
-fn evaluate(operators: &[Operator], operants: &[u64]) -> u64 {
-    let mut result = operants[0];
-    let mut iter = operants.iter();
-    iter.next(); // skip first
-    for (operant, operator) in iter.zip(operators) {
-        match operator {
-            Multiply => result *= operant,
-            Addition => result += operant,
-            Concat => result = u64::from_str(&format!("{}{}", result, operant)).unwrap()
-        }
-    }
-    result
-}
-
-fn generate_operator_vec(val: u64, len: usize) -> Vec<Operator> {
-    let mut result = Vec::new();
-    let mut val = val;
-    for _ in 0..len {
-        if val & 1 == 1 {
-            result.push(Multiply)
-        } else {
-            result.push(Addition)
-        }
-        val = val >> 1;
-    }
-    result
-}
-
-fn generate_operator_vec3(val: u64, len: usize) -> Vec<Operator> {
-    let mut result = Vec::new();
-    let mut val = val;
-    for _ in 0..len {
-        if val & 0b11 == 1 {
-            result.push(Multiply)
-        } else if val & 0b11 == 2 {
-            result.push(Addition)
-        } else if val & 0b11 == 3 {
-            result.push(Concat)
-        } else {
-            result.push(Addition)
-        }
-        val = val >> 2;
-    }
-    result
-}
-
-pub fn exec_day7_part2(input: &str) -> String {
-    let equations = input.lines().map(|l| {
+fn parse_input(input: &str) -> Vec<(u64, Vec<u64>)> {
+    input.lines().map(|l| {
         let numbers = l.split_whitespace().collect_vec();
         let mut result = numbers[0].to_string();
         result.pop();
         let result = u64::from_str(&result).unwrap();
-        let rest = numbers.iter().rev().take(numbers.len()-1).map(|n| u64::from_str(n).unwrap()).rev().collect_vec();
+        let rest = numbers.iter().rev().take(numbers.len() - 1).map(|n| u64::from_str(n).unwrap()).rev().collect_vec();
         (result, rest)
-    }).collect_vec();
-    let max_len = equations.iter().map(|(_, rest)| rest.len()).max().unwrap();
-    let mut operators = Vec::new();
-    for _ in 0..max_len {
-        operators.push(Multiply);
-        operators.push(Addition);
+    }).collect_vec()
+}
+
+fn concat(o1: u64, o2: u64) -> u64 {
+    (o1 * 10u64.pow(o2.ilog10()+1)) + o2
+}
+
+fn check2(operator: Operator, end_result: u64, current_result: u64, operants: &[u64]) -> bool {
+    if end_result == current_result && operants.is_empty() {
+        return true;
     }
+    if end_result < current_result || operants.is_empty() {
+        return false;
+    }
+    match operator {
+        Multiply => {
+            check2(Multiply, end_result, current_result * operants[0], &operants[1..]) ||
+                check2(Addition, end_result, current_result * operants[0], &operants[1..]) ||
+                check2(Concat, end_result, current_result * operants[0], &operants[1..])
+        }
+        Addition => {
+            check2(Multiply, end_result, current_result + operants[0], &operants[1..]) ||
+                check2(Addition, end_result, current_result + operants[0], &operants[1..]) ||
+                check2(Concat, end_result, current_result + operants[0], &operants[1..])
+        }
+        Concat => {
+            check2(Multiply, end_result, concat(current_result, operants[0]), &operants[1..]) ||
+                check2(Addition, end_result, concat(current_result, operants[0]), &operants[1..]) ||
+                check2(Concat, end_result, concat(current_result, operants[0]), &operants[1..])
+        }
+    }
+}
+
+pub fn exec_day7_part2(input: &str) -> String {
+    let equations = parse_input(input);
 
     equations.par_iter().map(|(result, operants)| {
-        if (0..2u64.pow(2 * operants.len() as u32)).par_bridge().any(|ops| {
-            evaluate(&generate_operator_vec3(ops, operants.len()), operants.as_slice()) == *result
-        }) {
+        if check2(Multiply, *result, operants[0], &operants[1..]) ||
+            check2(Addition, *result, operants[0], &operants[1..]) ||
+            check2(Concat, *result, operants[0], &operants[1..]){
             *result
         } else {
             0
@@ -129,4 +117,12 @@ fn test_day7_part2() {
         Err(_) => panic!(),
     };
     assert_eq!(exec_day7_part2(&input), "11387")
+}
+
+#[test]
+fn test_concat() {
+    assert_eq!(concat(123, 456), 123456);
+    assert_eq!(concat(123, 1), 1231);
+    assert_eq!(concat(123, 10), 12310);
+    assert_eq!(concat(123, 100), 123100);
 }
